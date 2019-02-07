1fe4f0afee899945881109cd4c2bb8fb019e5b51
ManashreeA <manashree_nerkar@yahoo.co.in>
2019-02-07 17:10:55 +0530

    Updated environment, Added deployment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  jsonapiURL : "http://35.245.70.23:5001/contactus/",
 };
